ec53677792e9f83b9b96c1275ab7997515cfea1f
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2024-03-10 17:43:51 +0530

    Added Python User IO Example

---
+++ python_input_output.py
@@ -0,0 +1,9 @@
+num1 = input('Please enter the first number: ')
+num2 = input('Please enter a second number: ')
+print(num1 + num2)
+print(int(num1) + int(num2))
+
+str1 = input('Please enter your first name: ')
+str2 = input('Please enter your second name: ')
+
+print('Hello {}, {}'.format(str1, str2))
