3a9e0a6695a43e24f15d7705ad587de1a5f7f8fa
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2024-03-10 15:28:26 +0530

    Initial commit

---
+++ python_strings.py
@@ -0,0 +1,15 @@
+a = "Hello World!"
+print(a)
+
+b = "This is too" \
+    "big to fit it a single line" \
+    "so you multi lined it"
+print(b)
+
+a = 'Hello '
+b = 'There!'
+print(a + b)
+
+name = 'John'
+print(name[0])
+print(len(name)) # len() function returns length of a String 
